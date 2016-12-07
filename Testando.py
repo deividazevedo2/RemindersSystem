af5a38460dd93a1a1e58b6b98c8f1a1ed2752882
deivid <deividazevedo1@gmail.com>
2016-12-07 17:18:51 -0300

    alterações em testes

--- Testando.py
+++ Testando.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import AnaliseDeComentarios
+import AnaliseDeComentarios, string
 
 #comentario com caractere especial q da problema quando eh convertido
 comentario = 'first.！dfeufhewf?'
@@ -33,5 +33,66 @@ for linha in palavras:
 
     comecar += 1
 
+def find(str, ch):
+    indice = 0
+    lista = []
+    while indice < len(str):
+        if str[indice] == ch:
+            lista.append(indice)
+        indice = indice + 1
+    return lista
 
-print s
+def verifica_coreid(texto, core):
+
+    coreIdInicio = '['
+    coreIdFim = ']'
+
+    # verificar se o coreId eh igual
+    if len(find(texto, coreIdInicio)) != 0:
+        listaabre = find(texto, coreIdInicio)
+        listafecha = find(texto, coreIdFim)
+
+        tamanho = len(listaabre)
+        contador = 0
+        while len(listaabre) > contador:
+            coreId = texto[listaabre[contador]+2:listafecha[contador]]
+            #texto = ' '.join(texto.split(coreId))
+            contador +=1
+            if (core == coreId):
+                return True
+        return False
+
+if __name__ == '__main__':
+
+    lista = [1, 2, 3, 4, 5]
+    print lista.__contains__(6)
+
+    # listaabre = []
+    # listafecha = []
+    #
+    # texto = "fdnoi [~deivid][~emenezes][~joao] dsgs "
+    # #rint texto.__contains__('[~')
+    # print verifica_coreid(texto, 'joao')
+
+    # abre = '{'
+    # fecha = '}'
+    #
+    # if find(texto,"{") != -1:
+    #     print 'entrou no if'
+    #     listaabre = find(texto,abre)
+    #     print len(listaabre)-1
+    #
+    #     listafecha = find(texto, fecha)
+    #     print listaabre, listafecha
+    #
+    #     tamanho = len(listaabre)
+    #     contador = 0
+    #     while len(listaabre) > 0:
+    #
+    #         excluir = texto[listaabre[contador]:listafecha[contador+1]+1]
+    #         texto = ' '.join(texto.split(excluir))
+    #         tamanho -= 2;
+    #         listaabre = find(texto, abre)
+    #         listafecha = find(texto, fecha)
+    #         print texto
+
