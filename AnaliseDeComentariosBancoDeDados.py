2bff12f16658f69f2f32f1df55ed969cc04971cc
deivid <deividazevedo1@gmail.com>
2016-12-07 17:21:05 -0300

    novos metodos de analise inseridos. Alterados alguns algoritmos de busca e comparação. Código OK

--- AnaliseDeComentariosBancoDeDados.py
+++ AnaliseDeComentariosBancoDeDados.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 import string
 
-
 def BoyerMooreHorspool(pattern, text):
     m = len(pattern)
     n = len(text)
@@ -18,37 +17,154 @@ def BoyerMooreHorspool(pattern, text):
     while k < n:
         j = m - 1; i = k
         while j >= 0 and text[i] == pattern[j]:
-            j -= 1; i -= 1
+            j -= 1;
+            i -= 1
         if j == -1:
             return i + 1
         k += skip[ord(text[k])]
     return -1
 
+def find(str, ch):
+    indice = 0
+    lista = []
+    while indice < len(str):
+        if str[indice] == ch:
+            lista.append(indice)
+        indice = indice + 1
+    return lista
 
-def analise(texto):
+def reduz_texto(texto):
 
-    safe_chars = string.ascii_letters + string.digits + ' '
+    abre = '{'
+    fecha = '}'
+
+
+    if len(find(texto, abre)) != 0:
+
+        listaabre = find(texto, abre)
+        listafecha = find(texto, fecha)
+
+        tamanho = len(listaabre)
+        contador = 0
+        while len(listaabre) > 0:
+            excluir = texto[listaabre[contador]:listafecha[contador + 1] + 1]
+            texto = ' '.join(texto.split(excluir))
+            tamanho -= 2;
+            listaabre = find(texto, abre)
+            listafecha = find(texto, fecha)
+
+            return texto
+    else:
+        return texto
+
+def verifica_coreid(texto, core):
+
+    coreIdInicio = '['
+    coreIdFim = ']'
+
+    # verificar se o coreId eh igual
+    if len(find(texto, coreIdInicio)) != 0:
+        listaabre = find(texto, coreIdInicio)
+        listafecha = find(texto, coreIdFim)
+
+        tamanho = len(listaabre)
+        contador = 0
+        while len(listaabre) > contador:
+            coreId = texto[listaabre[contador]+2:listafecha[contador]]
+            contador +=1
+            if (core == coreId):
+                return True
+        return False
+
+#contar quantidade de repeticoes
+def countPrefix(words, prefix):
+    return len([1 for w in words if w.startswith(prefix)])
+
+
+def analise(texto, coreId, listaDeRelevancias):
+
+    safe_chars = string.ascii_letters + string.digits + ' ' + '?' + '[' + ']' + '~' + '{' + '}'
     text = ''.join([char if char in safe_chars else ' ' for char in texto])
 
-    print text
+    text = reduz_texto(text)
 
     arq = open('words.txt', 'r')
     palavras = arq.readlines()
 
+    qtpattern = 0
+    valor = 0
+
     for linha in palavras:
+
+        linhacompleta = linha.strip().split(",")
 
         c = 0
         pos = 0
         texto = text.upper()[pos:len(text)]
-        pattern = linha.strip().upper()
+        pattern = linhacompleta[0].upper()
 
         while c < len(text):
 
             s = BoyerMooreHorspool(pattern.upper(), texto)
 
             if s > -1:
-                print 'Pattern \"' + pattern + '\" found at position', s + pos
 
+                qtpattern += 1
                 pos += s + len(pattern) + 1
                 texto = text.upper()[pos:len(text)]
+
+                valor = int(linhacompleta[1])
+
             c += 1
+
+    if text.__contains__('[~'):
+        if texto.find(coreId.upper()) > -1:
+            listaDeRelevancias.append('MENCIONA O DONO')
+        else:
+            listaDeRelevancias.append('NAO MENCIONA O DONO')
+            # listaDeRelevancias.append('MENCIONA O DONO')
+        # else:
+            # listaDeRelevancias.append('NAO MENCIONA O DONO')
+    else:
+        listaDeRelevancias.append('NAO MENCIONA NINGUEM')
+
+    # if texto.find(coreId.upper()) > -1:
+    #     listaDeRelevancias.append('MENCIONA O DONO')
+
+    if qtpattern == 1:
+
+        if valor == 3 or valor == 4 or valor == 5:
+            listaDeRelevancias.append("ALTA RELEVANCIA")
+        if valor == 2:
+            listaDeRelevancias.append("MEDIA RELEVANCIA")
+        if valor == 1:
+            listaDeRelevancias.append("POUCA RELEVANCIA")
+
+    if qtpattern >= 2:
+        if (qtpattern >= 3) | (qtpattern == 2 and valor != 3) | (qtpattern == 2 and valor == 3):
+            listaDeRelevancias.append("ALTA RELEVANCIA")
+
+    condicoes = ['NAO MENCIONA NINGUEM','NAO MENCIONA O DONO', 'MENCIONA O DONO','ALTA RELEVANCIA','MEDIA RELEVANCIA', 'POUCA RELEVANCIA']
+
+    resultado = [countPrefix(listaDeRelevancias, p) for p in condicoes]
+
+    return resultado
+
+def mencionou_usuario(resultado):
+    if resultado[2] > 0:
+        if resultado[3] == 0:
+            return False
+        else:
+            return True
+
+def analisando_prioridades(resultado):
+    if resultado[0] >= 0:
+        if resultado[3] > 0 or resultado[4] > 3 or resultado[5] > 3:
+            return True
+        else:
+            return False
+    if resultado[1] > 0:
+        if resultado[2] == 0:
+            return False
+        elif resultado[3] > 0 or resultado[4] > 3 or resultado[5] > 3:
+            return False
